91760b17fec36efbaac21defef7d65cf0fad0697
Vanna Huang <delightful1211@gmail.com>
2026-09-01 16:54:48 +0800

    feat: setup github actions deploy

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/VV/',
   server: {
     proxy: {
       '/api': {
         target: 'http://192.168.100.31:8000',
         changeOrigin: true
-        //changeOrigin:true是Vite Proxy在轉送API請求時,把HTTP Request裡面的Host標題改成後端伺服器位址。
       }
     }
   }
 })
-
-
